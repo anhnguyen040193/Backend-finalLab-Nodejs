6f72bbc3c4bf6ad35758b54162acc1179a643b6e
Anh Nguyen <anh.nguyen@terralogic.com>
2021-05-19 16:25:34 +0700

    up cluster+ dotenv

--- mongoose.js
+++ mongoose.js
@@ -4,10 +4,9 @@ const { createCategory } = require('./seeder/category');
 const { createProduct } = require('./seeder/product');
 const { createUser } = require('./seeder/user');
 const { loadCategory } = require('./test-case/loadCategory');
-const { URLdb = 'mongodb://anhnguyen:123@localhost:27017/nordic?authSource=admin' } = process.env;
-
+const {MONGO_URL} = process.env;
 const mongooseData = mongoose
-  .connect(URLdb, {
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
