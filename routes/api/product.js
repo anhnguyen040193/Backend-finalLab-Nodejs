d85c4e8bebac24d05b0524c9de4d6457f6347653
Anh Nguyen <anh.nguyen@terralogic.com>
2021-05-24 10:44:05 +0700

    api get one product

--- routes/api/product.js
+++ routes/api/product.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var mongoose = require('mongoose');
-const Products = require('../../model/product');
+var express = require("express");
+var mongoose = require("mongoose");
+const Products = require("../../model/product");
 var router = express.Router();
 
-router.get('/', async function (req, res, next) {
-  if(req.query.filter){
+router.get("/", async function (req, res, next) {
+  if (req.query.filter) {
     const queryReq = JSON.parse(req.query.filter);
     let query = {};
     if (queryReq.where.salePrice) {
@@ -14,36 +14,47 @@ router.get('/', async function (req, res, next) {
       if (err) return res.json(err);
       if (docs.length > 0) {
         res.send({
-          message: 'success get products',
+          message: "success get products",
           response: docs,
           totalLength: docs.length,
         });
       } else {
         res.send({
-          message: 'not found',
+          message: "not found",
         });
       }
     }).limit(parseInt(req.query.limit));
-  }else{
+  } else {
     await Products.find({}, function (err, docs) {
       if (err) return res.json(err);
       if (docs.length > 0) {
         res.send({
-          message: 'success get all products',
+          message: "success get all products",
           response: docs,
           totalLength: docs.length,
         });
       } else {
         res.send({
-          message: 'not found',
+          message: "not found",
         });
       }
-    })
+    });
   }
- 
 });
-
-router.post('/add/', function (req, res, next) {
+router.get("/:id", async function (req, res, next) {
+  try {
+    const getOneProduct = await Products.find({ _id: req.body._id });
+    res.send({
+      message: "success get product",
+      response: getOneProduct,
+    });
+  } catch (error) {
+    res.send({
+      error,
+    });
+  }
+});
+router.post("/add/", function (req, res, next) {
   const addValues = {
     _id: mongoose.Types.ObjectId(),
     name: req.body.name,
@@ -60,7 +71,7 @@ router.post('/add/', function (req, res, next) {
   res.redirect(`/admin/products/${addValues._id}`);
 });
 
-router.post('/update/', async function (req, res, next) {
+router.post("/update/", async function (req, res, next) {
   // console.log(req.body);
   // Products.find({ _id: req.body._id }, function (err, docs) {
   //   if (err) return res.json(err);
