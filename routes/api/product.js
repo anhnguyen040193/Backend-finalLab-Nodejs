0c3ec4bd6359cebecfec3c90e244c2c4ca7e431b
Anh Nguyen <anh.nguyen@terralogic.com>
2021-05-26 11:00:55 +0700

    update api product update

--- routes/api/product.js
+++ routes/api/product.js
@@ -92,33 +92,30 @@ router.post("/add/", function (req, res, next) {
 });
 
 router.post("/update/", async function (req, res, next) {
-  // console.log(req.body);
-  // Products.find({ _id: req.body._id }, function (err, docs) {
-  //   if (err) return res.json(err);
-  //   console.log('docs', docs);
-  // });
-  const addValues = {
-    _id: req.body._id,
-    name: req.body.name,
-    // image: req.body.image,
-    // thumbnail: req.body.thumbnail,
-    shortDescription: req.body.shortDescription,
-    // categoryId: req.body.categoryId,
-    salePrice: req.body.salePrice,
-    originalPrice: req.body.originalPrice,
-    // images: req.body.images,
-    // thumbnails: req.body.thumbnails,
-  };
-  await Products.updateOne(
-    { _id: req.body._id },
-    {
-      $set: addValues,
-    },
-    function (err, docs) {
-      if (err) return res.json(err);
-      res.redirect(`/admin/products/${addValues._id}`);
-    }
-  );
+  console.log(req.body);
+  try {
+    const addValues = {
+      _id: req.body._id,
+      name: req.body.name,
+      // image: req.body.image,
+      // thumbnail: req.body.thumbnail,
+      shortDescription: req.body.shortDescription,
+      categoryId: req.body.categoryId,
+      salePrice: req.body.salePrice,
+      originalPrice: req.body.originalPrice,
+      // images: req.body.images,
+      // thumbnails: req.body.thumbnails,
+    };
+    await Products.updateOne(
+      { _id: req.body._id },
+      {
+        $set: addValues,
+      }
+    );
+    res.send({ msg: "update successful" });
+  } catch (error) {
+    res.send({ error });
+  }
 });
 
 router.post(
